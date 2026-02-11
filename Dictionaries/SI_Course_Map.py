d9d2f41b72760f4034f529d93b15af018f29d9ab
Clara Smith <clarajs2017@outlook.com>
2026-02-10 19:59:07 -0600

    Added course code dictionary map, and added mapping functionality to the Organization notebook. CHEM  2113 remains an enigma.

---
+++ Dictionaries/SI_Course_Map.py
@@ -0,0 +1,146 @@
+course_map = {
+    "CHEM 1103": {
+        "New_Code": "CHEM 14103",
+        "Title": "University Chemistry I"
+    },
+    "CHEM 1123": {
+        "New_Code": "CHEM 14203",
+        "Title": "University Chemistry II"
+    },
+    "CHEM 1123H": {
+        "New_Code": "CHEM 142H3",
+        "Title": "Honors Chemistry II"
+    },
+    "CHEM 1073": {
+        "New_Code": "CHEM 12103",
+        "Title": "Fundamentals of Chemistry"
+    },
+    "CHEM 1073 Online": {
+        "New_Code": "CHEM 12103 Online",
+        "Title": "Fundamentals of Chemistry Online"
+    },
+    "CHEM 3603": {
+        "New_Code": "CHEM 36053",
+        "Title": "Organic Chemistry I"
+    },
+    "BIOL 1543": {
+        "New_Code": "BIOL 10103",
+        "Title": "Principles of Biology"
+    },
+    "CHEM 2613": {
+        "New_Code": "CHEM 26103",
+        "Title": "Organic Physiological Chemistry"
+    },
+    "CHEM 3613": {
+        "New_Code": "CHEM 36203",
+        "Title": "Organic Chemistry II"
+    },
+    "NURS 3314": {
+        "New_Code": "NURS 33174",
+        "Title": "Pathophysiology"
+    },
+    "UNIV 1001": {
+        "New_Code": "UNIV 10051",
+        "Title": "University Perspectives"
+    },
+    "ACCT 2013": {
+        "New_Code": "ACCT 20103",
+        "Title": "Accounting Principles"
+    },
+    "ACCT 2013 Online": {
+        "New_Code": "ACCT 20103 Online",
+        "Title": "Accounting Principles Online"
+    },
+    "ACCT 2023": {
+        "New_Code": "ACCT 20203",
+        "Title": "Accounting Principles II"
+    },
+    "ACCT 2023 Online": {
+        "New_Code": "ACCT 20203 Online",
+        "Title": "Accounting Principles II Online"
+    },
+    "ECON 2013": {
+        "New_Code": "ECON 21003",
+        "Title": "Principles of Macroeconomics"
+    },
+    "ECON 2013 Online": {
+        "New_Code": "ECON 21003 Online",
+        "Title": "Principles of Macroeconomics Online"
+    },
+    "ECON 2013H": {
+        "New_Code": "ECON 2100H3",
+        "Title": "Honors Principles of Macroeconomics"
+    },
+    "ECON 2023": {
+        "New_Code": "ECON 22003",
+        "Title": "Principles of Microeconomics"
+    },
+    "ECON 2023 Online": {
+        "New_Code": "ECON 22003 Online",
+        "Title": "Principles of Microeconomics Online"
+    },
+    "ECON 2023H": {
+        "New_Code": "ECON 220H3",
+        "Title": "Honors Principles of Microeconomics"
+    },
+    "ECON 2143": {
+        "New_Code": "ECON 21403",
+        "Title": "Basic Economics: Theory and Practice"
+    },
+    "ECON 2143 Online": {
+        "New_Code": "ECON 21403 Online",
+        "Title": "Basic Economics: Theory and Practice Online"
+    },
+    "ECON 2143H": {
+        "New_Code": "ECON 214H3",
+        "Title": "Honors Basic Economics: Theory and Practice"
+    },
+    "EXSC 3353": {
+        "New_Code": "EXSC 33503",
+        "Title": "Mechanics of Human Movement"
+    },
+    "CHEG 2113": {
+        "New_Code": "CHEG 21103",
+        "Title": "Introduction to Chemical Engineering I"
+    },
+    "CHEG 2133": {
+        "New_Code": "CHEG 21303",
+        "Title": "Fluid Mechanics"
+    },
+    "MATH 2043": {
+        "New_Code": "MATH 22003",
+        "Title": "Survey of Calculus"
+    },
+    "WCOB 1033": {
+        "New_Code": None,
+        "Title": "Data Analysis"
+    },
+    "CHEM 3703": {
+    "New_Code": "CHEM 37073",
+    "Title": "Organic Chemistry I Lecture for Chemistry Majors"
+    },
+    "CHEM 3603H": {
+        "New_Code": "CHEM 36053",
+        "Title": "Honors Organic Chemistry I"
+    },
+    "MEEG 2703": {
+        "New_Code": "MEEG 27003",
+        "Title": "Computer Methods in Mechanical Engineering"
+    },
+    "MEEG 3013": {
+        "New_Code": "MEEG 30103",
+        "Title": "Mechanics of Materials"
+    },
+    "MEEG 3113": {
+        "New_Code": "MEEG 31103",
+        "Title": "Fundamentals of Vibrations"
+    },
+    "MEGG 2003": {
+        "New_Code": "MEEG 20003",
+        "Title": "Statics"
+    },
+    "CHEM 2113": {
+    "New_Code": None,  # No current equivalent.
+    "Title": "CHEM 2113 (Name unknown.)"
+    },
+}
